8fe4ec76b24c1a7422c0287566943489981d4278
yangzilong <yangzilong@richinfo.cn>
2026-03-16 00:20:32 +0800

    feat: 测试登录接口

--- api/routes/adminAuth.ts
+++ api/routes/adminAuth.ts
@@ -4,7 +4,6 @@ import * as authController from "../controllers/adminAuthController";
 const router = Router();
 
 // 对应前端的 login 路径
-router.get("/login",  (req, res) => res.send('Server is running'))
 router.post("/login", authController.login);
 router.post("/refresh-token", authController.handleRefreshToken);
 
